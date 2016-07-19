b79a175e05166358f25d6a0f323d77c4b63cc49a
Brian Grant and Caleb Stokka <brian+caleb@>
2016-07-19 09:59:50 -0700

    add products to product page

--- app.rb
+++ app.rb
@@ -2,6 +2,22 @@ require('pg')
 require("sinatra")
 require("sinatra/reloader")
 require("sinatra/activerecord")
-require("./lib/")
+require("./lib/product")
 require("./lib/purchase")
 also_reload("lib/**/*.rb")
+
+get('/') do
+  erb(:index)
+end
+
+get('/products') do
+  @products = Product.all()
+  erb(:products)
+end
+
+post('/product') do
+  name = params.fetch("new_product_name")
+  price = params.fetch("new_product_price")
+  product = Product.create({:name => name, :price => price, :purchase_id => nil, :sold => false})
+  redirect('/products')
+end
